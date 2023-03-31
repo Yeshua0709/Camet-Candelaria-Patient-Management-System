1e99f66aec9d3c648a741e1560fd6afcbbe560ee
Yeshua0709 <abrenica.y@gmail.com>
2023-03-31 13:07:09 +0800

    added sidebar component

--- patient-management-system/pages/dashboard/Overview.js
+++ patient-management-system/pages/dashboard/Overview.js
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Sidebar  from './Sidebar'
 export default function OverView(){
     return (
+
+
         <div>This is the OverView Page</div>
     )
 }
--- patient-management-system/pages/dashboard/index.js
+++ patient-management-system/pages/dashboard/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import OverView from './Overview'
-
+import Sidebar from './Sidebar'
 export default function Dashboard(props){
     return (
      
       <div>
+          <Sidebar />
           <OverView />
       </div>
      
