1ea9ed828dc15e18fca5c53cd27487a59d274085
Miguel <juanmiguelcaparas76@gmail.com>
2023-03-31 21:23:22 +0800

    More Sprint Objectives

--- patient-management-system/pages/landing/index.js
+++ patient-management-system/pages/landing/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Logo from './landingimgs/logo2.png'
 import Image from 'next/image'
-
+import Girl from './landingimgs/girl.png'
 
 
 
@@ -49,12 +49,143 @@ export default function LandingPage(){
                                 height="auto"
                                 />
                             </div>
+
                     </div>
                 </div>
             </div>
 
 
-           
+            <div className='landgrp2'>
+                <div className='landcontainer2'>
+                    <div className='landheader2'>
+                        <p> Services</p>
+                    </div>
+                    <div className='landflexbox2'>
+                        <div className='landbox1'>
+                            <div className='header2_1'>
+                                <p>Preventive Care</p>
+                            </div>
+                            <ul>
+                                <li>&bull; Checkups</li>
+                                <li>&bull; Cleanings</li>
+                                <li>&bull; X-Ray</li>
+                                <li>&bull; Dental Sealant</li>
+                                <li>&bull; Fluoride Treatment</li>
+                            </ul>
+                        </div>
+
+                        <div className='landbox1'>
+                            <div className='header2_1'>
+                                <p>Restorative Care</p>
+                            </div>
+                            <ul>
+                                <li>&bull; Fillings</li>
+                                <li>&bull; Crowns</li>
+                                <li>&bull; Bridges</li>
+                                <li>&bull; Root Canal Therapy</li>
+                            </ul>
+                        </div>
+
+                        <div className='landbox1'> 
+                            <div className='header2_1'>
+                                <p>Cosmetic Dentistry</p>
+                            </div>
+                            <ul>
+                                <li>&bull; Teeth Whitening</li>
+                                <li>&bull; Veneers</li>
+                                <li>&bull; Braces</li>
+                                <li>&bull; Aligners</li>
+                            </ul>
+                        </div>
+        
+                    </div>
+                </div>
+            </div>
+
+            <div className='landgrp3'>
+                <div className='landcontainer3'>
+                    <div className='landflexbox3'>
+                        <div className='header3'>
+                            <p>About Us</p>
+                        </div>
+                        <div className='text3'>
+                            <p>The dental clinic is a team of friendly and compassionate dental professionals dedicated to providing high-quality dental care services to their patients. They offer a wide range of dental services, including preventive care, complex restorative treatments, and cosmetic dentistry services. The clinic is equipped with state-of-the-art facilities and accepts most insurance plans, providing flexible payment options to fit patients' budgets and schedules.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='landgrp4'>
+                <div className='landcontainer4'>
+                             
+                    <div className='landflexbox4'>
+                       
+                        <div className='landwindow4_1'>
+                            <div className='landh4'>
+                                <p>BOOK YOUR APPOINTMENT</p>
+                            </div>
+
+                            <div className='landform'>
+                                <label for='landfname'>First Name</label>
+                                <br></br>
+                                <input type='text' id='landfname' name='fname'></input>
+
+                                <br></br>
+
+                                <label for='landlname'>Last Name</label>
+                                <br></br>
+                                <input type='text' id='landlname' name='lname'></input>
+
+                                <br></br>
+
+                                <label for='landlname'>Gender</label>
+                                <br></br>
+                                <select id="cars" name="cars">
+                                    <option value="volvo">Male</option>
+                                    <option value="saab">Female</option>
+                                    <option value="saab">Others</option>
+                                </select>
+
+                                
+                                <br></br>
+
+                                <label for='landemail'>Email</label>
+                                <br></br>
+                                <input type='text' id='landemail' name='email'></input>
+
+                                <br></br>
+
+                                <label for='landnnum'>Number</label>
+                                <br></br>
+                                <input type='text' id='landnum' name='num'></input>
+
+                                <br></br>
+                                <br></br>
+                                <input type="submit" value="Submit"></input>
+                            </div>
+                        </div>
+                        <div className='landwindow4_2'>
+                                <Image className='rightlogo'
+                                src={Girl}
+                                alt="Picture of the author"
+                                width="auto"
+                                height="auto"
+                                />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
 
 
          </div>
